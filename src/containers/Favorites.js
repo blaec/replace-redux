b1470696feb54dcb06947b97bac09ce09ae80e2e
blaec <odeskonst@yandex.ru>
2020-03-29 21:44:10 +0300

    469. Toggling Favorites with the Context API

--- src/containers/Favorites.js
+++ src/containers/Favorites.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { useSelector } from 'react-redux';
-
+import {ProductContext} from "../context/product-context";
 import FavoriteItem from '../components/Favorites/FavoriteItem';
 import './Products.css';
 
 const Favorites = props => {
-  const favoriteProducts = useSelector(state =>
-    state.shop.products.filter(p => p.isFavorite)
-  );
+  const favoriteProducts = useContext(ProductContext).products.filter(p => p.isFavorite);
   let content = <p className="placeholder">Got no favorites yet!</p>;
   if (favoriteProducts.length > 0) {
     content = (
